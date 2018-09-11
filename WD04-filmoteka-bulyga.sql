f02ddb3cfa2dbabbc3667b474bb13f55ba8e5ca6
Anatoliy <anatoliynbulyga@gmail.com>
2018-09-11 20:40:05 +0300

    add new data base for edit

--- WD04-filmoteka-bulyga.sql
+++ WD04-filmoteka-bulyga.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Сен 11 2018 г., 11:05
+-- Время создания: Сен 11 2018 г., 20:38
 -- Версия сервера: 5.5.45-log
 -- Версия PHP: 5.6.12
 
@@ -32,17 +32,16 @@ CREATE TABLE IF NOT EXISTS `films` (
   `genre` text NOT NULL,
   `year` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=32 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=49 ;
 
 --
 -- Дамп данных таблицы `films`
 --
 
 INSERT INTO `films` (`id`, `title`, `genre`, `year`) VALUES
-(1, 'Облачный Атлас', 'драма', 2012),
-(2, 'Такси 2', 'комедия', 2000),
-(27, 'Аватар', 'драма', 2012),
-(31, 'Чужой', 'ужасы', 1990);
+(42, 'Такси 3', 'комедия', 2010),
+(47, 'Такси 4 вторая часть', 'фантастика', 2017),
+(48, 'Такси 4 первая часть', 'комедия', 2005);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
